587e4961191ab80c6e934ee47dab44a4bd522423
Waraidako <waraidako.god@gmail.com>
2025-09-10 00:16:41 +0300

    Slight margin tweak

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const directories: string[] = parseDirectories('files').sort();
 export default function Home() {
   return (
       <div className="m-[1.5em] flex-col justify-items-center">
-          <a href={"https://files.servermaksa.ru/2025-09-09_20-05-14.mp4"} className={"flex justify-center text-2xl w-full font-montserrat"}>Собрание по ВКРБ</a>
+          <a href={"https://files.servermaksa.ru/2025-09-09_20-05-14.mp4"} className={"flex justify-center text-2xl w-full font-montserrat mb-5"}>Собрание по ВКРБ</a>
           {
               directories.map(((directory: string, index: number) => (
                   <Subject key={index} subjectName={directory} />
